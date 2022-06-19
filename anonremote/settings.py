f74bdfb5b957eebbf6a886c3c003523f5d8962d4
Mike2015 <olumichael2015@outlook.com>
2022-06-19 21:52:36 +0100

    'fist'

--- anonremote/settings.py
+++ anonremote/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(f'{BASE_DIR}/home', "static"),
+    os.path.join(f'{BASE_DIR}/remote', "static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
